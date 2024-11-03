15c5594a79880055c3e99f2bf651cdefc183d5e6
ShafayetAhmad <shafayet.ahmad1@gmail.com>
2024-11-03 18:05:00 +0000

    home content component created and restructured

--- maftuh-frontend/src/app/page.tsx
+++ maftuh-frontend/src/app/page.tsx
@@ -1,6 +1,5 @@
+import HomeContentComp from "@/components/homecontentcomp/HomeContentComp";
 import LeftNavbar from "@/components/leftnavbar/LeftNavbar";
-import Post from "@/components/post/Post";
-import PostCreator from "@/components/postcreator/PostCreator";
 import RightSidebar from "@/components/rightsidebar/RightSidebar";
 
 export default function Home() {
@@ -9,72 +8,7 @@ export default function Home() {
       <LeftNavbar />
 
       <main className="flex-grow max-w-2xl mx-auto border-l border-r border-gray-200">
-        <PostCreator></PostCreator>
-        <div>
-          <Post
-            username="John Doe"
-            handle="johndoe"
-            timestamp="2h"
-            content="This is a sample post content for Twitter clone."
-            avatarUrl="/public/icon_m.jpg"
-          />
-          <Post
-            username="Jane Smith"
-            handle="janesmith"
-            timestamp="4h"
-            content="Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component."
-            avatarUrl="/public/icon_m.jpg"
-          />
-          <Post
-            username="Jane Smith"
-            handle="janesmith"
-            timestamp="4h"
-            content="Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component."
-            avatarUrl="/public/icon_m.jpg"
-          />
-          <Post
-            username="Jane Smith"
-            handle="janesmith"
-            timestamp="4h"
-            content="Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component."
-            avatarUrl="/public/icon_m.jpg"
-          />
-          <Post
-            username="Jane Smith"
-            handle="janesmith"
-            timestamp="4h"
-            content="Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component."
-            avatarUrl="/public/icon_m.jpg"
-          />
-          <Post
-            username="Jane Smith"
-            handle="janesmith"
-            timestamp="4h"
-            content="Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component."
-            avatarUrl="/public/icon_m.jpg"
-          />
-          <Post
-            username="Jane Smith"
-            handle="janesmith"
-            timestamp="4h"
-            content="Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component."
-            avatarUrl="/public/icon_m.jpg"
-          />
-          <Post
-            username="Jane Smith"
-            handle="janesmith"
-            timestamp="4h"
-            content="Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component."
-            avatarUrl="/public/icon_m.jpg"
-          />
-          <Post
-            username="Jane Smith"
-            handle="janesmith"
-            timestamp="4h"
-            content="Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component."
-            avatarUrl="/public/icon_m.jpg"
-          />
-        </div>
+        <HomeContentComp></HomeContentComp>
       </main>
 
       <RightSidebar />
---
+++ maftuh-frontend/src/components/homecontentcomp/HomeContentComp.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import PostCreator from '../postcreator/PostCreator';
+import Post from '../post/Post';
+
+const HomeContentComp = () => {
+    return (
+      <div>
+        <PostCreator></PostCreator>
+        <div>
+          <Post
+            username="John Doe"
+            handle="johndoe"
+            timestamp="2h"
+            content="This is a sample post content for Twitter clone."
+            avatarUrl="/public/icon_m.jpg"
+          />
+          <Post
+            username="Jane Smith"
+            handle="janesmith"
+            timestamp="4h"
+            content="Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component."
+            avatarUrl="/public/icon_m.jpg"
+          />
+          <Post
+            username="Jane Smith"
+            handle="janesmith"
+            timestamp="4h"
+            content="Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component."
+            avatarUrl="/public/icon_m.jpg"
+          />
+          <Post
+            username="Jane Smith"
+            handle="janesmith"
+            timestamp="4h"
+            content="Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component."
+            avatarUrl="/public/icon_m.jpg"
+          />
+          <Post
+            username="Jane Smith"
+            handle="janesmith"
+            timestamp="4h"
+            content="Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component."
+            avatarUrl="/public/icon_m.jpg"
+          />
+          <Post
+            username="Jane Smith"
+            handle="janesmith"
+            timestamp="4h"
+            content="Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component."
+            avatarUrl="/public/icon_m.jpg"
+          />
+          <Post
+            username="Jane Smith"
+            handle="janesmith"
+            timestamp="4h"
+            content="Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component."
+            avatarUrl="/public/icon_m.jpg"
+          />
+          <Post
+            username="Jane Smith"
+            handle="janesmith"
+            timestamp="4h"
+            content="Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component."
+            avatarUrl="/public/icon_m.jpg"
+          />
+          <Post
+            username="Jane Smith"
+            handle="janesmith"
+            timestamp="4h"
+            content="Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component.Here's another sample post to showcase the component."
+            avatarUrl="/public/icon_m.jpg"
+          />
+        </div>
+      </div>
+    );
+};
+
+export default HomeContentComp;
